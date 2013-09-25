62ec233a9e1a73fe8e74f93c8510fdb87ac79fd0
James Sweet <james.sweet88@googlemail.com>
2013-09-25 14:20:00 -0400

    Added decompiling of old and new files to allow for comparison

--- compile.py
+++ compile.py
@@ -6,6 +6,9 @@ import py_compile
 import zipfile
 import glob
 import fnmatch
+import dis
+import marshal
+import sys
 
 # Remove any previous directory
 if os.path.exists("res_mods"):
@@ -20,12 +23,33 @@ shutil.copytree("res/scripts", "res_mods/0.8.8/scripts")
 
 # Compile Files
 for root, dirnames, filenames in os.walk('res_mods'):
-  for filename in fnmatch.filter(filenames, '*.py'):
-      py_compile.compile(os.path.join(root, filename), doraise=True)
+	for filename in fnmatch.filter(filenames, '*.py'):
+		print os.path.join(root.replace("res_mods/0.8.8/","res/"), filename.replace(".py","-old.byte"))
+
+		sys.stdout = open(os.path.join(root.replace("res_mods/0.8.8/","res/"), filename.replace(".py","-old.byte")), "w")
+		with open(os.path.join(root.replace("res_mods/0.8.8/","res/"), filename.replace(".py",".pyc")), "rb") as pyc_reader:
+			pyc_reader.read(8)
+			dis.dis(marshal.load(pyc_reader))
+		sys.stdout.close()
+
+		py_compile.compile(os.path.join(root, filename), doraise=True)
+
+		sys.stdout = open(os.path.join(root.replace("res_mods/0.8.8/","res/"), filename.replace(".py","-new.byte")), "w")
+		with open(os.path.join(root, filename.replace(".py",".pyc")), "rb") as pyc_reader:
+			pyc_reader.read(8)
+			dis.dis(marshal.load(pyc_reader))
+		sys.stdout.close()
+
+		sys.stdout = sys.__stdout__
 
 # Zip Package
 fZip = zipfile.ZipFile( "WOTDecompiled.zip", "w" )
 for root, dirnames, filenames in os.walk('res_mods'):
-  for filename in fnmatch.filter(filenames, '*.pyc'):
-	fZip.write(os.path.join(root, filename))
+	for filename in fnmatch.filter(filenames, '*.pyc'):
+		fZip.write(os.path.join(root, filename))
 fZip.close()
+
+def decompile(pycfile):
+	with open(pycfile, "rb") as pyc_reader:
+		pyc_reader.read(8)
+		dis.dis(marshal.load(pyc_reader))
